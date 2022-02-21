cfc52a9fbf73413e28c56a52eab451d80ada2349
Kale Leach <kleach1212@gmail.com>
2022-02-21 14:32:45 -0800

    commit tree 556a5a3e24659fe87a56800594ab2b2514412232

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+
+    const compNum = target - array[i];
+
+    for ( let j = i + 1; j < array.length; j++) {
+      if( array[j] === compNum) 
+        return true
+    }
+  }
+  return false;
 }
+
+
 
 /* 
   Write the Big O time complexity of your function here
